1b1372b909529c27ed6124c7edceccdc03b6b097
koojahyeob <gjm01083145028@gmail.com>
2023-09-02 00:13:13 +0900

    first commit

--- 2579.py
+++ 2579.py
@@ -8,13 +8,13 @@ for _ in range(n): # 계단 입력 받기
     stair.append(int(input()))
     
 def climbstairs(n): # 계단 dp 점화식 만들기
-    if n == 1:
+    if n == 1: #초기값 설정
         d[0] = stair[0]
         return d[0]
-    elif n == 2:
+    elif n == 2: #초기값 설정2
         d[1] = stair[0] + stair[1]
         return d[1]
-    for i in range (2,n):
+    for i in range (2,n): #점화식 생성
         d[0] = stair[0]
         d[1] = stair[0]+stair[1]
         d[i] = max(d[i-3]+stair[i-1]+stair[i], d[i-2]+stair[i])
